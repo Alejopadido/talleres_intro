21c91e6233e1aa62595da811a519bc187c66f4c2
Alejopadido <alejopadido13@gmail.com>
2023-09-19 18:02:17 -0500

    UwU

--- 05/solucion.cpp
+++ 05/solucion.cpp
@@ -1,41 +1,79 @@
+// Integrantes:
+// - Alejandro Parrado
+// - Sebastian Vargas
+
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-void solve();
+int revisar(int nSoldados, int nSubordinaciones, int filaindia[], int comandantes[], int subordinados[])
+{
+    int posComandante=0, posSubordinado=0, buenaSubordinacion=0, malaSubordinacion=0, malOrdenComandante=0, malOrdenSubordinado=0;
+    for(int i=0; i<nSubordinaciones; i++)
+    {
+        posComandante=0, posSubordinado=0, buenaSubordinacion=0, malOrdenComandante=0, malOrdenSubordinado=0;
+        for(int j=0; j<nSoldados; j++)
+        {
+            if(comandantes[i]==filaindia[j])
+            {
+                posComandante=j;
+            }
+            if(subordinados[i]==filaindia[j])
+            {
+                posSubordinado=j;
+            }
+            if(posComandante<posSubordinado)
+            buenaSubordinacion++;
+            if(posComandante>posSubordinado)
+            {
+                malOrdenComandante=comandantes[i];
+                malOrdenSubordinado=subordinados[i];
+            }
+        }
 
-int main() {
-    int t;
-    cin >> t;
-
-    while(t--) {
-        solve();
     }
-
+    if(buenaSubordinacion==nSubordinaciones-1)
+    {
+        cout<<"El orden es correcto. "<<endl;
+    }
+    else if(buenaSubordinacion<nSubordinaciones-1)
+    {
+        malaSubordinacion=1;
+    }
+    if(malaSubordinacion==1)
+    {
+        cout<<"El orden es incorrecto. La pareja mal ordenada es: ";
+        cout<<malOrdenComandante<<" - "<<malOrdenSubordinado<<endl;
+    }
     return 0;
 }
 
-void solve() {
-    int n, m;
-    cin >> n;
+int main()
+{
+    int n, k, l;
+    ifstream soldados("soldados.txt");
+    soldados>>n;
+    for(int i=1; i<=n; i++)
+    {
+        int nSoldados=0;
+        soldados>>nSoldados;
+        int filaindia[nSoldados];
+        for(k=0; k<nSoldados; k++)
+        {
+            soldados>>filaindia[k];
+        }
+        int nSubordinaciones;
+        soldados>>nSubordinaciones;
+        int comandantes[nSubordinaciones];
+        int subordinados[nSubordinaciones];
+        for(l=0; l<nSubordinaciones; l++)
+        {
+            soldados>>comandantes[l];
+            soldados>>subordinados[l];
+        }
 
-    int fila_india[n];
-
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        fila_india[i] = x;
+        revisar(nSoldados,nSubordinaciones,filaindia,comandantes,subordinados);
     }
-
-     cin >> m;
-
-     int comandantes[m], subordinados[m];
-
-     for (int i = 0; i < m; i++) {
-        int c, s;
-        cin >> c >> s;
-        comandantes[i] = c;
-        subordinados[i] = s;
-     }
-
+    return 0;
 }
